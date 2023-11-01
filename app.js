c9ef8b78f052b5f90f963ab9717826c1d301b0e0
Annisa Nur Ramadhani <annisannor014@gmail.com>
2023-11-01 18:38:28 +0700

    update code

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ const app = express();
 app.use(express.json());
 app.use('/api/users', userRoutes);
 
+app.get('/', (req, res) => {
+    res.json({ message: 'Hello, World!' });
+});
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
 
